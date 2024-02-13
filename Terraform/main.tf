402307a8a30387c1e2b1d3401d6b8bfb939fd3f7
Shani <shaniben5253@gmail.com>
2024-02-13 12:05:05 +0200

    ggg

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,3 +12,13 @@ terraform {
 provider "aws" {
   region = "eu-west-1"
 }
+
+
+resource "aws_instance" "shani" {
+  ami           = "0e731c8a588258d0d"
+  instance_type = "t2.xlarge"
+
+  tags = {
+    Name = "ShaniInstance"
+  }
+}
