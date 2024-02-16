8c7cf28513c56d49017535f800953eac9430592d
Shani <shaniben5253@gmail.com>
2024-02-16 12:19:50 +0200

    ggg

--- Terraform/api-gateway.tf
+++ Terraform/api-gateway.tf
@@ -1,17 +1,34 @@
 
-    integrations = {
-    "ANY /" = {
-      lambda_arn             = module.aws_lambda_function.myapp.arn
+
+module "api_gateway" {
+  source = "terraform-aws-modules/apigateway-v2/aws"
+
+  name          = "dev-http"
+  description   = "My awesome HTTP API Gateway"
+  protocol_type = "HTTP"
+
+
+  # Access logs ????
+  default_stage_access_log_destination_arn = "arn:aws:logs:eu-west-1:835367859851:log-group:debug-apigateway"
+  default_stage_access_log_format          = "$context.identity.sourceIp - - [$context.requestTime] \"$context.httpMethod $context.routeKey $context.protocol\" $context.status $context.responseLength $context.requestId $context.integrationErrorMessage"
+
+
+  # Routes and integrations
+  integrations = {
+    "POST /" = {
+      lambda_arn             = aws_lambda_function.myapp.role
       payload_format_version = "2.0"
       timeout_milliseconds   = 12000
+    }
+
+
+    "$default" = {
+      lambda_arn = aws_lambda_function.myapp.role
     }
   }
 
 
-
-
-
-
-
-
-
+  tags = {
+    Name = "http-apigateway"
+  }
+}
--- Terraform/lambda-bucket.tf
+++ Terraform/lambda-bucket.tf
@@ -1,5 +1,3 @@
-
-
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket        = shani_bucket
   force_destroy = true
