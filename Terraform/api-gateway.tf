f43752740ac2143e021d24e93417b7dad1f6d31c
Shani <shaniben5253@gmail.com>
2024-02-16 12:28:31 +0200

    ggg

--- Terraform/api-gateway.tf
+++ Terraform/api-gateway.tf
@@ -8,11 +8,6 @@ module "api_gateway" {
   protocol_type = "HTTP"
 
 
-  # Access logs ????
-  default_stage_access_log_destination_arn = "arn:aws:logs:eu-west-1:835367859851:log-group:debug-apigateway"
-  default_stage_access_log_format          = "$context.identity.sourceIp - - [$context.requestTime] \"$context.httpMethod $context.routeKey $context.protocol\" $context.status $context.responseLength $context.requestId $context.integrationErrorMessage"
-
-
   # Routes and integrations
   integrations = {
     "POST /" = {
