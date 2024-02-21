77355b28c53c85f1501d3d9c69c3db80b5326bdb
Shani <shaniben5253@gmail.com>
2024-02-21 10:55:23 +0200

    changes

--- Terraform/api-gateway.tf
+++ Terraform/api-gateway.tf
@@ -1,45 +1,78 @@
-resource "aws_api_gateway_rest_api" "my_api" {
-  name        = "MyAPI"
-  description = "My REST API"
+resource "aws_apigatewayv2_api" "main" {
+  name          = "main"
+  protocol_type = "HTTP"
 }
 
-resource "aws_api_gateway_resource" "my_resource" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
-  path_part   = "webhook"
+resource "aws_apigatewayv2_stage" "shani" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  name        = "shani"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.main_api_gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
 }
 
-resource "aws_api_gateway_method" "my_post_method" {
-  rest_api_id   = aws_api_gateway_rest_api.my_api.id
-  resource_id   = aws_api_gateway_resource.my_resource.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
+resource "aws_cloudwatch_log_group" "main_api_gw" {
+  name = "/aws/api-gw/${aws_apigatewayv2_api.main.name}"
 
-resource "aws_api_gateway_integration" "my_lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  resource_id = aws_api_gateway_resource.my_resource.id
-  http_method = aws_api_gateway_method.my_post_method.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.myapp.invoke_arn
+  retention_in_days = 14
 }
 
 
-resource "aws_api_gateway_deployment" "my_deployment" {
-  depends_on = [
-    aws_api_gateway_integration.my_lambda_integration
-  ]
+#______________________________________________________________________________
 
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  stage_name  = "prod"
+resource "aws_apigatewayv2_integration" "lambda_hello" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  integration_uri    = aws_lambda_function.myapp.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "get_hello" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_hello.id}"
+}
+
+resource "aws_apigatewayv2_route" "post_hello" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  route_key = "POST /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_hello.id}"
 }
 
 resource "aws_lambda_permission" "api_gw" {
-  statement_id  = "AllowAPIGatewayInvoke"
+  statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.myapp.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.my_api.execution_arn}/prod/POST/webhook"
+
+  source_arn = "${aws_apigatewayv2_api.main.execution_arn}/*/*"
 }
+
+output "hello_base_url" {
+  value = aws_apigatewayv2_stage.shani.invoke_url
+}
+
+
+
+
+
--- Terraform/github.tf
+++ Terraform/github.tf
@@ -2,7 +2,7 @@ resource "github_repository_webhook" "webhook" {
   repository = "Pro"
 
   configuration {
-    url          = "${aws_api_gateway_rest_api.my_api.execution_arn}/${aws_api_gateway_deployment.my_deployment.stage_name}/${aws_api_gateway_resource.my_resource.path_part}"
+    url          = aws_apigatewayv2_stage.shani.invoke_url
 
     content_type = "form"
   }
