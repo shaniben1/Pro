115b7479cde214f9b1af3c1d979f14b74d633c7e
Shani <shaniben5253@gmail.com>
2024-02-27 19:42:36 +0200

    changes

--- Terraform/github.tf
+++ Terraform/github.tf
@@ -2,10 +2,12 @@ resource "github_repository_webhook" "webhook" {
   repository = "Pro"
 
   configuration {
-    url          = "https://cawl94idt3.execute-api.us-east-1.amazonaws.com/dev/webhook"
+    #url          = "https://cawl94idt3.execute-api.us-east-1.amazonaws.com/dev/webhook"
+    url =aws_lambda_permission.api_gw.source_arn
     insecure_ssl = false
     content_type = "json"
 
   }
   events = ["pull_request"]
 }
+
