4c52c82de5d854ce0614f11363b79c53964c1a76
Shani <shaniben5253@gmail.com>
2024-02-28 20:52:36 +0200

    changesgk

--- CloudwatchLoger/myapp.py
+++ CloudwatchLoger/myapp.py
@@ -4,7 +4,8 @@ def handler(event, context):
     # Extract information from GitHub webhook payload
     try:
         payload = json.loads(event['body'])
-        repo_name = payload(['repository']['name'])
+        print("Received event:", json.dumps(event))
+#        repo_name = payload(['repository']['name'])
 
  #       if 'commits' in payload:
  #           for commit in payload['commits']:
@@ -25,7 +26,7 @@ def handler(event, context):
             if changed_files:
                 log_message = "Changed files:\n" + '\n'.join(changed_files)
                 print(log_message)  # This will be logged in CloudWatch Logs
-                print("the repository name : {}" .format(repo_name))
+#                print("the repository name : {}" .format(repo_name))
             else:
                 print("No files have been changed.")
         else:
