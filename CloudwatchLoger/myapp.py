1fc23e940b329aa419e98ef5d53d4bd25ea73426
Shani <shaniben5253@gmail.com>
2024-02-29 15:15:13 +0200

    project

--- CloudwatchLoger/myapp.py
+++ CloudwatchLoger/myapp.py
@@ -1,53 +1,25 @@
 import json
+
+
+
+
+
 def handler(event, context):
-    # Extract information from GitHub webhook payload
-    try:
+    # Print the entire event object to see the structure
+    print("Received event:", json.dumps(event))
 
-        payload = json.dumps(event)
-        print("Received event: {} ", payload)           # print("Received event:", json.dumps(event))
-#        repo_name = payload(['repository']['name'])
-
-        if 'commits' in payload:
-            changed_files = []
-
-            for commit in payload['commits']:
-                changed_files.extend(commit['added'])
-                changed_files.extend(commit['modified'])
-                changed_files.extend(commit['removed'])
- #       changed_files = []
-  #      if 'commits' in payload:
-  #          for commit in payload['commits']:
-  #               if 'added' in commit:
-  #                  changed_files.extend(commit['added'])
-   #              if 'modified' in commit:
-   #                  changed_files.extend(commit['modified'])
-   #              if 'removed' in commit:
-   #                  changed_files.extend(commit['removed'])
-
-            # Log changed files to CloudWatch Logs
-            if changed_files:
-                log_message = "Changed files:\n" + '\n'.join(changed_files)
-                print(log_message)  # This will be logged in CloudWatch Logs
-#                print("the repository name : {}" .format(repo_name))
-            else:
-                print("No files have been changed.")
-        else:
-            print("No commit information found in payload.")
-
-    except Exception as e:
-        print(f"Error: {e}")
-        return {
-            'statusCode': 500,
-            'body': json.dumps('Internal Server Error')
-        }
+    # Check if the event contains a body
+    if 'body' in event:
+        # Parse the body from JSON to Python dictionary
+        body = json.loads(event['body'])
+        print("Received payload:", json.dumps(body))
+    else:
+        print("No payload found in the event body")
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Success')
+        'body': json.dumps('Payload printed successfully')
     }
-
-
-
 
 
 
