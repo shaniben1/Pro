8d316871c61630481ad4db1dab650c179beceff3
Shani <shaniben5253@gmail.com>
2024-02-29 17:50:21 +0200

    project

--- CloudwatchLoger/myapp.py
+++ CloudwatchLoger/myapp.py
@@ -1,43 +1,42 @@
 import json
 import urllib.request  # library to send requests and get json
+
+
 def handler(event, context):
-
-
-
-
-
-
-
-
-
-
     # Print the entire event object to see the structure
     print("Received event:", json.dumps(event))
 
     # Check if the event contains a body
     if 'body' in event:
+        # action = closed merge_at !=null
         # Parse the body from JSON to Python dictionary
-        body_dict = event['body']
-        sha = body_dict['pull_request']['head']['sha']
-        repo_name = body_dict['pull_request']['head']['repo']['name']
+        body_dict = json.loads(event['body'])
+        action = body_dict['action']
+        merged = body_dict['pull_request']['merged']
 
-        # Extract commits_url from the event body
-        commits_url = body_dict['repository']['commits_url']
-        commits_url = commits_url[:-6]  # Remove "{/sha}" from the end of URL
-        url = f'{commits_url}/{sha}'
-        print(url)
+        if action == 'closed' and merged:
+            sha = body_dict['pull_request']['head']['sha']
+            repo_name = body_dict['pull_request']['head']['repo']['name']
 
-        # Send GET request using urllib
-        req = urllib.request.Request(url)
-        with urllib.request.urlopen(req) as response:
-            resp = response.read().decode('utf-8')
+            # Extract commits_url from the event body
+            commits_url = body_dict['repository']['commits_url']
+            commits_url = commits_url[:-6]  # Remove "{/sha}" from the end of URL
+            url = f'{commits_url}/{sha}'
+            print(url)
 
-        resp_json = json.loads(resp)
-        files = resp_json['files']
+            # Send GET request using urllib
+            req = urllib.request.Request(url)
+            with urllib.request.urlopen(req) as response:
+                resp = response.read().decode('utf-8')
 
-        print(f'Repository {repo_name} was changed:')
-        for file in files:
-            print(f"{file['filename']} was {file['status']}")
+            resp_json = json.loads(resp)
+            files = resp_json['files']
+
+            print(f'Repository {repo_name} was changed:')
+            for file in files:
+                print(f"{file['filename']} was {file['status']}")
+        else:
+            print('pull request is not closed yet')
     else:
         print("No payload found in the event body")
 
@@ -45,3 +44,4 @@ def handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Payload printed successfully')
     }
+
