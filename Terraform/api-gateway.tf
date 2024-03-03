d71b2c6815117d71c9f16c042b31a21e95ab1dc2
Shani <shaniben5253@gmail.com>
2024-03-03 08:43:57 +0200

    change

--- Terraform/api-gateway.tf
+++ Terraform/api-gateway.tf
@@ -3,7 +3,6 @@ resource "aws_api_gateway_rest_api" "my_api" {
   name        = "rest_api"
 }
 
-
 resource "aws_api_gateway_resource" "my_resource" {
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
@@ -13,7 +12,7 @@ resource "aws_api_gateway_resource" "my_resource" {
 resource "aws_api_gateway_method" "my_method" {
   rest_api_id   = aws_api_gateway_rest_api.my_api.id
   resource_id   = aws_api_gateway_resource.my_resource.id
-  http_method   = "POST" #GET
+  http_method   = "POST"
   authorization = "NONE"
 }
 
